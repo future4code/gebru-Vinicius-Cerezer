3fce6bc47c6c38d9d474facc59d036d1328d305f
unknown <codecerezer@gmail.com>
2021-11-09 22:39:42 -0300

    Aula de Javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,30 @@
+// Exercicio 1
+// let nome
+// let idade
+// console.log(typeof nome,idade)
+
+// let nome = Prompt ("Qual o teu nome?")
+// let idade = prompt ("Qual a tua idade?")
+// console.log (typeof nome)
+// console.log (typeof idade)
+
+// let nomeUser
+// let idadeUser
+// console.log ("olá", nome, "Você tem", idade, "anos de vida")
+
+// Exercicio 2
+// let Academia = prompt ("Você faz academia?")
+// let Ler = prompt ("Você lê livros?")
+// let Namora = prompt ("Você namora?")
+
+// console.log (Academia)
+// console.log (Ler)
+// console.log (Namora)
+
+//Execicio 3
+
+// const a = 10
+// const b = 25
+
+// console.log (typeof "a , b")
+
