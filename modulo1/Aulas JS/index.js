d1e4c36609945f1639b99ac03b05d7b2504ac253
unknown <codecerezer@gmail.com>
2021-11-11 18:44:20 -0300

    Aula JS

---
+++ modulo1/Aulas JS/index.js
@@ -0,0 +1,63 @@
+// Exercicio 1
+
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. ", false)
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", false) 
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", true)
+
+// console.log("d. ", typeof Boolean)
+
+//Exercicio 2
+
+// let A  = parseInt (prompt ("Digite um numero!"))
+// let  B = parseInt (prompt ("Digite outro numero!"))
+
+// const soma = A + B
+// console.log(A + B)
+
+// Exercicio 3
+
+//Você pode colocar parseInt antes do prompt para não concatenar os numneros e sim somar, pois ele "cancela" o string.
+
+// Exercicios de Escrita de Codigo.
+// Exercicio 1
+
+// let A  = parseInt (prompt ("Digite sua idade!"))
+// let B = parseInt (prompt ("Digite a do seu melhor amigo!"))
+
+// const resultado = A - B
+// console.log ("Sua idade é maior do que a do seu melhor amigo?")
+// console.log (A - B)
+
+//Exercicio 2 
+
+// let A  = parseInt (prompt ("Digite um numero par"))
+// const B = 2
+
+// const divisão = A / B
+// console.log (divisão)
+
+// Exercicio 3
+
+// let A  = parseInt (prompt ("Digite sua idade!"))
+
+// const B = 24
+// const C = 30
+// const D = 12
+
+// let Horas = A * B
+// let Dias = B * C
+// let Meses = C * D
+
+// console.log(Horas, Dias, Meses)
+
+// Exercicio 4
+
